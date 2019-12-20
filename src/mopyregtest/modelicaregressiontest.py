fc69b8fa835dd3d158814e70b797439c87fab1e6
Philipp Emanuel Stelzig <software@philippstelzig.de>
2019-12-20 16:08:35 +0100

    Made the tool argument in the constructor of RegressionTest and optional argument by giving it a default value.
    If no argument for tool is given in the constructor of RegressionTest,
    then the class will look through the PATH variable of the machine searching
    for omc and dymola and execute the tests for all of the found simulators.
    
    Nicer output formatting to make results of test output more readable.

--- src/mopyregtest/modelicaregressiontest.py
+++ src/mopyregtest/modelicaregressiontest.py
@@ -18,7 +18,7 @@ class RegressionTest:
     Creates OpenModelica-compatible .mos scripts to import and simulate the model with .csv output.
     The .csv output is then compared against a reference result, possibly only on a subset of columns.
     """
-    def __init__(self, package_folder, model_in_package, result_folder, tool="omc"):
+    def __init__(self, package_folder, model_in_package, result_folder, tool=None):
         """
         Constructor of the RegresssionTest class.
 
@@ -31,25 +31,20 @@ class RegressionTest:
         result_folder :
             Path of the folder where the output of the model testing shall be written to
         tool:
-            Simulator used to translate, compile and execute Modelica model. Default is omc,
-            other option is dymola.
+            Simulator used to translate, compile and execute Modelica model. Valid options are omc,
+            and dymola. If no argument is specified, RegressionTest will search the PATH variable
+            for both omc and dymola and will execute the tests for all of the two that is found. 
         """
 
         self.template_folder_path = pathlib.Path(__file__).parent.absolute() / "templates"
         self.package_folder_path = pathlib.Path(package_folder).absolute()
         self.model_in_package = model_in_package
         self.result_folder_path = pathlib.Path(result_folder).absolute()
-        self.tool = tool
 
-        self.tool_executable = self.tool
-        if platform.system() == 'Windows':
-            self.tool_executable += ".exe"
-
-        self.model_import_template = self.tool + "/model_import.mos.template"
-        self.model_simulate_template = self.tool + "/model_simulate.mos.template"
-        self.model_import_mos = "model_import.mos"
-        self.model_simulate_mos = "model_simulate.mos"
-        self.tool_output = self.tool + "_output.txt"
+        if tool != None:
+            self.tools = [tool]
+        else:
+            self.tools = [tl for tl in ["omc", "dymola"] if shutil.which(tl) != None]
 
         self.result_folder_created = False
 
@@ -70,7 +65,7 @@ class RegressionTest:
         return
 
     def import_and_simulate(self):
-        print("Simulating model {}".format(self.model_in_package))
+        print("Simulating model {} using the simulation tools: {}" .format(self.model_in_package, ", ".join(self.tools)))
 
         # Create folder where output of the simulation shall be stored
         if not self.result_folder_path.exists():
@@ -133,57 +128,71 @@ class RegressionTest:
         return
 
     def _run_model(self):
-        if self.tool == "omc":
-            # Copy mos templates to result folder
-            shutil.copy(self.template_folder_path / self.model_import_template, self.result_folder_path / self.model_import_mos)
-            shutil.copy(self.template_folder_path / self.model_simulate_template, self.result_folder_path / self.model_simulate_mos)
 
-            # Modify the import template
-            repl_dict = {}
-            repl_dict["PACKAGE_FOLDER"] = str(self.package_folder_path.as_posix())
-            repl_dict["RESULT_FOLDER"] = str(self.result_folder_path.as_posix())
-            repl_dict["MODEL_IN_PACKAGE"] = self.model_in_package
-
-            RegressionTest.replace_in_file(self.result_folder_path / self.model_import_mos, repl_dict)
-
-            # Run the import script and write the output of the OpenModelica Compiler (omc) to omc_output
-            os.system(self.tool_executable + " {} > {}".format(self.model_import_mos, self.tool_output))
-
-            # Read simulation options from the simulation_options_file
-            model_import_output_file = open(self.tool_output, 'r')
-            omc_messages = model_import_output_file.readlines()
-            model_import_output_file.close()
-
-            (start_time, stop_time, tolerance, num_intervals, interval) = omc_messages[-1].lstrip('(').rstrip(')').split(',')
-
-            # Modify the simulation template
+        for tool in self.tools:
+            tool_executable = tool
             if platform.system() == 'Windows':
-                repl_dict["SIMULATION_BINARY"] = "{}.exe".format(self.model_in_package)
-            elif platform.system() == 'Linux':
-                repl_dict["SIMULATION_BINARY"] = "./{}".format(self.model_in_package)
-            repl_dict["START_TIME"] = start_time
-            repl_dict["STOP_TIME"] = stop_time
-            repl_dict["TOLERANCE"] = tolerance
-            repl_dict["NUM_INTERVALS"] = num_intervals
+                tool_executable += ".exe"
 
-            RegressionTest.replace_in_file(self.result_folder_path / self.model_simulate_mos, repl_dict)
+            print("Using simulation tool {}".format(tool_executable))
 
-            # Run the simulation script and append the output of the OpenModelica Compiler (omc) to omc_output
-            os.system(self.tool_executable + " {} >> {}".format(self.model_simulate_mos, self.tool_output))
+            model_import_template = tool + "/model_import.mos.template"
+            model_simulate_template = tool + "/model_simulate.mos.template"
+            model_import_mos = "model_import.mos"
+            model_simulate_mos = "model_simulate.mos"
+            tool_output = tool + "_output.txt"
 
-        if self.tool == "dymola":
-            # Copy mos templates to result folder
-            shutil.copy(self.template_folder_path / self.model_simulate_template, self.result_folder_path / self.model_simulate_mos)
+            if tool == "omc":
+                # Copy mos templates to result folder
+                shutil.copy(self.template_folder_path / model_import_template, self.result_folder_path / model_import_mos)
+                shutil.copy(self.template_folder_path / model_simulate_template, self.result_folder_path / model_simulate_mos)
 
-            # Modify the simulation template
-            repl_dict = {}
-            repl_dict["PACKAGE_FOLDER"] = str(self.package_folder_path.as_posix())
-            repl_dict["RESULT_FOLDER"] = str(self.result_folder_path.as_posix())
-            repl_dict["MODEL_IN_PACKAGE"] = self.model_in_package
+                # Modify the import template
+                repl_dict = {}
+                repl_dict["PACKAGE_FOLDER"] = str(self.package_folder_path.as_posix())
+                repl_dict["RESULT_FOLDER"] = str(self.result_folder_path.as_posix())
+                repl_dict["MODEL_IN_PACKAGE"] = self.model_in_package
 
-            RegressionTest.replace_in_file(self.result_folder_path / self.model_simulate_mos, repl_dict)
+                RegressionTest.replace_in_file(self.result_folder_path / model_import_mos, repl_dict)
 
-            # Run the simulation script
-            os.system(self.tool_executable + " {} /nowindow >> {}".format(self.model_simulate_mos, self.tool_output))
+                # Run the import script and write the output of the OpenModelica Compiler (omc) to omc_output
+                os.system(tool_executable + " {} > {}".format(model_import_mos, tool_output))
+
+                # Read simulation options from the simulation_options_file
+                model_import_output_file = open(tool_output, 'r')
+                omc_messages = model_import_output_file.readlines()
+                model_import_output_file.close()
+
+                (start_time, stop_time, tolerance, num_intervals, interval) = omc_messages[-1].lstrip('(').rstrip(')').split(',')
+
+                # Modify the simulation template
+                if platform.system() == 'Windows':
+                    repl_dict["SIMULATION_BINARY"] = "{}.exe".format(self.model_in_package)
+                elif platform.system() == 'Linux':
+                    repl_dict["SIMULATION_BINARY"] = "./{}".format(self.model_in_package)
+                repl_dict["START_TIME"] = start_time
+                repl_dict["STOP_TIME"] = stop_time
+                repl_dict["TOLERANCE"] = tolerance
+                repl_dict["NUM_INTERVALS"] = num_intervals
+
+                RegressionTest.replace_in_file(self.result_folder_path / model_simulate_mos, repl_dict)
+
+                # Run the simulation script and append the output of the OpenModelica Compiler (omc) to omc_output
+                os.system(tool_executable + " {} >> {}".format(model_simulate_mos, tool_output))
+
+            if tool == "dymola":
+                # Copy mos templates to result folder
+                shutil.copy(self.template_folder_path / model_simulate_template, self.result_folder_path / model_simulate_mos)
+
+                # Modify the simulation template
+                repl_dict = {}
+                repl_dict["PACKAGE_FOLDER"] = str(self.package_folder_path.as_posix())
+                repl_dict["RESULT_FOLDER"] = str(self.result_folder_path.as_posix())
+                repl_dict["MODEL_IN_PACKAGE"] = self.model_in_package
+
+                RegressionTest.replace_in_file(self.result_folder_path / model_simulate_mos, repl_dict)
+
+                # Run the simulation script
+                os.system(tool_executable + " {} /nowindow >> {}".format(model_simulate_mos, tool_output))
 
         return
