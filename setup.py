c81186d5e9ff6c17d837cf6b0cdbf2b3a2d014dc
Philipp Emanuel Stelzig <software@philippstelzig.de>
2020-06-12 16:16:24 +0200

    Created a setup.py to allow installation via pip.

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MoPyRegtest",
+    version="0.0.1",
+    author="Philipp Emanuel Stelzig",
+    description="A Python enabled simple regression testing framework for Modelica models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/pstelzig/mopyregtest",
+    packages=setuptools.find_packages(),
+    package_data={"mopyregtest": ["templates/omc/model_import.mos.template",
+                                  "templates/omc/model_simulate.mos.template"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux"
+    ],
+    python_requires='>=3.6'
+)
