434cbaa9e00e0e12c20bee5660f2b39a9be4d381
Philipp Emanuel Stelzig <software@philippstelzig.de>
2023-04-26 18:52:14 +0200

    Added unittests to check for matching start time and end time when unifying timestamps

--- tests/test_compare_results.py
+++ tests/test_compare_results.py
@@ -53,3 +53,19 @@ class TestCompareResults(unittest.TestCase):
 
         self.assertIsNone(pd.testing.assert_frame_equal(res1_ext_expect, results_ext[0]))
         self.assertIsNone(pd.testing.assert_frame_equal(res2_ext_expect, results_ext[1]))
+
+    def test_unify_timestamps_start_times(self):
+        res1 = pd.DataFrame(data=[[0.1, 1.0, 2.0], [0.5, 2.0, 4.0], [0.75, 3.0, 6.0], [1.0, 4.0, 8.0]],
+                            columns=["time", "quant1", "quant2"])
+        res2 = pd.DataFrame(data=[[0.0, 1.0, 2.0], [0.25, 1.5, 3.0], [0.5, 2.0, 4.0], [1.0, 4.0, 8.0]],
+                            columns=["time", "quant1", "quant2"])
+
+        self.assertRaises(ValueError, mopyregtest.RegressionTest._unify_timestamps, [res1, res2])
+
+    def test_unify_timestamps_end_times(self):
+        res1 = pd.DataFrame(data=[[0.0, 1.0, 2.0], [0.5, 2.0, 4.0], [0.75, 3.0, 6.0], [1.0, 4.0, 8.0]],
+                            columns=["time", "quant1", "quant2"])
+        res2 = pd.DataFrame(data=[[0.0, 1.0, 2.0], [0.25, 1.5, 3.0], [0.5, 2.0, 4.0], [0.9, 4.0, 8.0]],
+                            columns=["time", "quant1", "quant2"])
+
+        self.assertRaises(ValueError, mopyregtest.RegressionTest._unify_timestamps, [res1, res2])
