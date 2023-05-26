c6e57d15054b532adc0d119a69eb61ad4c3d3944
Philipp Emanuel Stelzig <software@philippstelzig.de>
2023-05-26 12:05:43 +0200

    Added a unit test for strategy to fill in timestamps with different number of occurrences
    in different results to be compared

--- tests/test_compare_results.py
+++ tests/test_compare_results.py
@@ -69,3 +69,34 @@ class TestCompareResults(unittest.TestCase):
                             columns=["time", "quant1", "quant2"])
 
         self.assertRaises(ValueError, mopyregtest.RegressionTest._unify_timestamps, [res1, res2])
+
+    def test_unify_timestamps_multiplicity(self):
+        res1 = pd.DataFrame(data=[[0.0, 1.0, 2.0],
+                                  [0.5, 2.0, 4.0], [0.5, 2.2, 4.4], [0.5, 2.4, 4.8],
+                                  [0.75, 3.0, 6.0],
+                                  [1.0, 4.0, 8.0]],
+                            columns=["time", "quant1", "quant2"])
+        res2 = pd.DataFrame(data=[[0.0, 1.0, 2.0], [0.0, 1.1, 2.1],
+                                  [0.25, 1.5, 3.0], [0.25, 1.6, 3.3],
+                                  [0.5, 2.0, 4.0], [0.5, 2.7, 4.7],
+                                  [1.0, 4.0, 8.0]],
+                            columns=["time", "quant1", "quant2"])
+
+        res1_ext_expect = pd.DataFrame(data=[[0.0, 1.0, 2.0], [0.0, 1.0, 2.0],
+                                             [0.25, 1.0, 2.0], [0.25, 1.0, 2.0],
+                                             [0.5, 2.0, 4.0], [0.5, 2.2, 4.4], [0.5, 2.4, 4.8],
+                                             [0.75, 3.0, 6.0],
+                                             [1.0, 4.0, 8.0]],
+                            columns=["time", "quant1", "quant2"])
+
+        res2_ext_expect = pd.DataFrame(data=[[0.0, 1.0, 2.0], [0.0, 1.1, 2.1],
+                                             [0.25, 1.5, 3.0], [0.25, 1.6, 3.3],
+                                             [0.5, 2.0, 4.0], [0.5, 2.7, 4.7], [0.5, 2.7, 4.7],
+                                             [0.75, 2.7, 4.7],
+                                             [1.0, 4.0, 8.0]],
+                            columns=["time", "quant1", "quant2"])
+
+        results_ext = mopyregtest.RegressionTest._unify_timestamps([res1, res2])
+
+        self.assertIsNone(pd.testing.assert_frame_equal(res1_ext_expect, results_ext[0]))
+        self.assertIsNone(pd.testing.assert_frame_equal(res2_ext_expect, results_ext[1]))
